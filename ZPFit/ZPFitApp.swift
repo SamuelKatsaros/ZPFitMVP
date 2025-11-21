133b6785743646550678f97029eb049348c883e1
SamuelKatsaros <150530309+SamuelKatsaros@users.noreply.github.com>
2025-11-21 12:35:40 -0500

    Initial Commit

---
+++ ZPFit/ZPFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZPFitApp.swift
+//  ZPFit
+//
+//  Created by Samuel E Katsaros on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ZPFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZPFitTests/ZPFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZPFitTests.swift
+//  ZPFitTests
+//
+//  Created by Samuel E Katsaros on 11/21/25.
+//
+
+import Testing
+@testable import ZPFit
+
+struct ZPFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
